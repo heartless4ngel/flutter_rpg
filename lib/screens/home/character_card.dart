7e80c5f29d582250908cc86a67589358cb4cf1b0
heartless4ngel <mattialancellotta@hotmail.com>
2024-03-14 21:46:23 +0100

    first commit

---
+++ lib/screens/home/character_card.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rpg/models/character.dart';
+import 'package:flutter_rpg/shared/styled_text.dart';
+import 'package:flutter_rpg/theme.dart';
+
+class CharacterCard extends StatelessWidget {
+  const CharacterCard(this.character, {super.key});
+
+  final Character character;
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 8),
+        child: Row(
+          children: [
+            Image.asset(
+              "assets/img/vocations/${character.vocation.image}",
+              width: 80,
+            ),
+            const SizedBox(
+              width: 20,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                StyledHeading(character.name),
+                StyledText(character.vocation.title),
+              ],
+            ),
+            const Expanded(
+              child: SizedBox(),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.arrow_forward),
+              color: AppColors.textColor,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/home/home.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rpg/models/character.dart';
+import 'package:flutter_rpg/screens/home/character_card.dart';
+import 'package:flutter_rpg/shared/styled_button.dart';
+import 'package:flutter_rpg/shared/styled_text.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const StyledTitle("My characters"),
+        centerTitle: true,
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: characters.length,
+                itemBuilder: (_, index) {
+                  return CharacterCard(characters[index]);
+                },
+              ),
+            ),
+            StyledButton(
+                onPressed: () {}, child: const StyledHeading("Create new")),
+          ],
+        ),
+      ),
+    );
+  }
+}
