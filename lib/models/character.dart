80399b5262040d888323d31da044c765fdfdea66
heartless4ngel <mattialancellotta@hotmail.com>
2025-05-29 00:08:49 +0200

    added vocation enhanced. added skill class

--- lib/models/character.dart
+++ lib/models/character.dart
@@ -1,7 +1,11 @@
+import 'package:flutter_rpg/models/skill.dart';
 import 'package:flutter_rpg/models/stats.dart';
+import 'package:flutter_rpg/models/vocation.dart';
 
 class Character with Stats {
   //fields
+  final Set<Skill> skills = {};
+  final Vocation vocation;
   final String name;
   final String slogan;
   final String id;
@@ -9,7 +13,7 @@ class Character with Stats {
 
   //constructor
   Character({
-    required this.name, required this.slogan, required this.id
+    required this.name, required this.slogan, required this.vocation, required this.id
   });
 
   //getters
@@ -17,6 +21,11 @@ class Character with Stats {
 
   void toggleIsFav() {
     _isFav = !_isFav;
+  }
+
+  void updateSkill(Skill skill){
+    skills.clear();
+    skills.add(skill);
   }
 }
 
