9a5b12c705042256154bb823e0f33f870a76383d
heartless4ngel <mattialancellotta@hotmail.com>
2025-05-30 00:01:05 +0200

    feat(screen): create screen "create"

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_rpg/screens/home/home.dart';
+import 'package:flutter_rpg/screens/create/create.dart';
 import 'package:flutter_rpg/theme.dart';
 
-import 'models/character.dart';
-
 void main() {
-  runApp( MaterialApp(home: Home(),theme: primaryTheme,));
+  runApp( MaterialApp(home: Create(),theme: primaryTheme,));
 }
 
 class Sandbox extends StatelessWidget {
