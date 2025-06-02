c26780e54b90af22993a1a5499d1c1dee90c97d2
heartless4ngel <mattialancellotta@hotmail.com>
2025-06-02 23:48:01 +0200

    chore(): set up firebase database

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,18 @@ import 'package:flutter_rpg/screens/home/home.dart';
 import 'package:flutter_rpg/services/character_store.dart';
 import 'package:flutter_rpg/theme.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(
     ChangeNotifierProvider(
       create: (context) => CharacterStore(),
       child: MaterialApp(home: Home(), theme: primaryTheme),
-
     ),
   );
 }
